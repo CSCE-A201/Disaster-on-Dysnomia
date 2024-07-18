593b736c18e8ff5f99c20fbf28f5624b362cf422
thatcherty <ryeking19@gmail.com>
2024-07-18 14:26:54 -0800

    Adding main and class definition templates

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include "classes.cpp"
+using namespace std;
+
+
+int main() {
+
+    Copilot copilot; 
+    Cockpit cockpit;
+    SleepingQuarters sleepingQuarters;
+    MedicalBay medicalBay;
+    StorageRoom storageRoom;
+    EngineRoom engineRoom;
+    Hallway hallway;
+    Player player;
+
+    srand(static_cast<unsigned int>(time(0))); // Seed the random number generator
+
+    // Display welcome message and introduction
+    cout << "Welcome to the Untitled RPG game!\n";
+    cout << readFromFile("introduction.txt");
+
+    // Main game loop
+    while (true)
+    {
+        hallway.move();
+
+        // Handle room entry based on player's choice
+        switch (hallway.getChoice())
+        {
+            case 'A':
+            case 'a':
+                player.set_location(COCKPIT);
+                cockpit.enter(copilot, engineRoom, player);
+                break;
+            case 'B':
+            case 'b':
+                player.set_location(SLEEPING_QUARTERS);
+                sleepingQuarters.enter(copilot, engineRoom, player);
+                break;
+            case 'C':
+            case 'c':
+                player.set_location(MEDICAL_BAY);
+                medicalBay.enter(copilot, engineRoom, player);
+                break;
+            case 'D':
+            case 'd':
+                player.set_location(STORAGE);
+                storageRoom.enter(copilot, engineRoom, player);
+                break;
+            case 'E':
+            case 'e':
+                player.set_location(ENGINE_ROOM);
+                engineRoom.enter(copilot, player);
+                break;
+            default:
+                cout << "Invalid choice. Please try again.\n";
+        }
+    }
+    return 0;
+}
